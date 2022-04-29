d1090982ac6ce07befb785776b5bf90bea4c987a
JPZEBRA <84015964+JPZEBRA@users.noreply.github.com>
2022-04-29 14:24:08 +0900

    Add files via upload

--- shop.py
+++ shop.py
@@ -1,20 +1,38 @@
 # 趣味のPython学習　Project 01-03
 # 雑貨屋パイソンちゃん
-# ばーじょん 0.1.1
+# ばーじょん 0.1.2
 
 from shopitem import CommonItem 
 from shopitem import FoodItem 
 from shopitem import ToyItem 
 from shopitem import FoodAndToyItem 
 
-x1 = CommonItem(name="Spoon",price=200)
-x1.show()
+itemlist = []
 
-x2 = FoodItem(name="Bread",price=300,date="2022/05/10")
-x2.show()
+# 仕入れ
 
-x3 = ToyItem(name="Minicar",price=500,age=6)
-x3.show()
+while len( nm := input("Item : ") ) > 0 :
+        try :
+                while ( pr := int(input("Price : "))) <= 0 : pass
+                dt = input("Date to use : ")
+                while ( len( ag := input("Age : "))) > 0:
+                        if int(ag)>0 : break
+        except ValueError:
+                pass
+        else :
+                if len(dt) == 0 and len(ag) == 0 :
+                        itemlist.append(CommonItem(name=nm,price=pr))
+                if len(dt) > 0 and len(ag) == 0 :
+                        itemlist.append(FoodItem(name=nm,price=pr,date=dt))
+                if len(dt) == 0 and len(ag) > 0 :
+                        itemlist.append(ToyItem(name=nm,price=pr,age=ag))
+                if len(dt) > 0 and len(ag) > 0 :
+                        itemlist.append(FoodAndToyItem(name=nm,price=pr,date=dt,age=ag))
 
-x4 = FoodAndToyItem(name="PrizeInFood",price=300,date="2022/07/10",age=5)
-x4.show()
+# 在庫確認
+
+for item in itemlist :
+        print("********************")
+        item.show()
+
+input("HIT ANY KEY")
