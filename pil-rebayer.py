ea10c92edfc35a4cddf7e57ac84ddff969c7059c
JPZEBRA <84015964+JPZEBRA@users.noreply.github.com>
2022-05-16 21:22:02 +0900

    Add files via upload

--- pil-rebayer.py
+++ pil-rebayer.py
@@ -3,9 +3,9 @@
 # COLOR-DATA TO BAYERD-DATA
 # CONVERT PROGRAM
 #   ( WITH PIL )
-# ばーじょん 1.0.1
+# ばーじょん 1.0.2
 
-ver = "1.0.1"
+ver = "1.0.2"
 
 from io import BytesIO
 
@@ -14,20 +14,45 @@ from PIL import ImageFile
 
 def rdd(img,x,y) :
     if x < 0 :
-        return 0
+        return (0,0,0)
     if y < 0 :
-        return 0
+        return (0,0,0)
     if x >= img.size[0] :
-        return 0
+        return (0,0,0)
     if y >= img.size[1] :
-        return 0
+        return (0,0,0)
     return img.getpixel((x,y))
+
+def rebayer00RGGB(img,x,y) :
+
+    rgb = rdd(img,x,y)
+
+    dt11 = (rgb[0]+rgb[1]+rgb[2]) // 3
+
+    return (dt11,dt11,dt11,dt11)
+
 
 def rebayer2x2RGGB(img,x,y) :
 
     rgb = rdd(img,x,y)
 
     return (rgb[0],rgb[1],rgb[1],rgb[2])
+
+def rebayer2x2RGGBmini(img,x,y) :
+
+    rgb = rdd(img,x+0,y+0)
+
+    if x%2==0 and y%2==0 :
+        return (rgb[0])
+    if x%2==0 and y%2==1 :
+        return (rgb[1])
+    if x%2==1 and y%2==0 :
+        return (rgb[1])
+    if x%2==1 and y%2==1 :
+        return (rgb[2])
+
+    return (0)
+
 
 print(f"*** REBAYER PROGRAM ( with PIL ) VERSION:{ver}")
 print("\nSELECT COLOR FILE\n")
@@ -57,7 +82,15 @@ while len( fnm := input("file : ") ) > 0 :
             print(f"{fnm} : not color file.")
             continue
 
-        md = 2
+        print("*** CONVERT MODE ***")
+        print("1: GRAY     ( 2x image )")
+        print("2: RGGB 2x2 ( 2x image )")
+        print("3: RGGB 2x2 ( 1x image )")
+
+        while True :
+            md = int(input("MODE :"))
+            if 1 <= md and md <= 3 :
+                break
 
 #       RE OPEN AS A BINARY
 
@@ -65,16 +98,30 @@ while len( fnm := input("file : ") ) > 0 :
             data = f.read()
             img_src = Image.open(BytesIO(data))
 
+        if md == 1 or md == 2 :
+            wd = wd * 2
+            ht = ht * 2
+
 #       CONVERT DATA
-        img_cv = Image.new('L',(img_src.size[0]*2,img_src.size[1]*2))
+        img_cv = Image.new('L',(wd,ht))
 
         for y in range(img_src.size[1]) :
             for x in range(img_src.size[0]) :
-                bb  = rebayer2x2RGGB(img_src,x,y)
-                img_cv.putpixel((x*2+0,y*2+0),bb[0])
-                img_cv.putpixel((x*2+1,y*2+0),bb[1])
-                img_cv.putpixel((x*2+0,y*2+1),bb[2])
-                img_cv.putpixel((x*2+1,y*2+1),bb[3])
+                if md == 1:
+                    bb  = rebayer00RGGB(img_src,x,y)
+                    img_cv.putpixel((x*2+0,y*2+0),bb[0])
+                    img_cv.putpixel((x*2+1,y*2+0),bb[1])
+                    img_cv.putpixel((x*2+0,y*2+1),bb[2])
+                    img_cv.putpixel((x*2+1,y*2+1),bb[3])
+                if md == 2:
+                    bb  = rebayer2x2RGGB(img_src,x,y)
+                    img_cv.putpixel((x*2+0,y*2+0),bb[0])
+                    img_cv.putpixel((x*2+1,y*2+0),bb[1])
+                    img_cv.putpixel((x*2+0,y*2+1),bb[2])
+                    img_cv.putpixel((x*2+1,y*2+1),bb[3])
+                if md == 3:
+                    bb  = rebayer2x2RGGBmini(img_src,x,y)
+                    img_cv.putpixel((x+0,y+0),bb)
 
         fno = fnm + ".rebayered-" + str(md) + ".png"
         print(f"WRITE OUT : {fno}")
